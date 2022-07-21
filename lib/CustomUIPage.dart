5f33819ae2f6d203e54f8fd833948deb55369274
huangyahong <huangyahong@joyy.com>
2022-07-21 21:20:58 +0800

    custom ui

--- lib/CustomUIPage.dart
+++ lib/CustomUIPage.dart
@@ -11,9 +11,56 @@ class CustomUIPage extends StatefulWidget {
 
 class _MyHomePageState extends State<CustomUIPage>
     with SingleTickerProviderStateMixin {
+  //使用控制Tabbar切换
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError("");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.system_update), text: "组合"),
+            Tab(icon: Icon(Icons.cake), text: "自绘")
+          ],
+          controller: _tabController,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          ListView(
+            children: <Widget>[
+              UpdatedItemWidget(
+                model: UpdatedItemModel(
+                    appIcon: "assets/icon.png",
+                    appDescription:
+                        "Thanks for using Google Maps! This release brings bug fixes that improve our product to help you discover new places and navigate to them.",
+                    appName: "Google Maps - Transit & Fond",
+                    appSize: "137.2",
+                    appVersion: "Version 5.19",
+                    appDate: "2019年6月5日"),
+                onPressed: () {},
+              )
+            ],
+          ),
+          Center(child: Cake())
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
 
@@ -24,7 +71,73 @@ class UpdatedItemWidget extends StatelessWidget {
   const UpdatedItemWidget(
       {Key? key, required this.model, required this.onPressed})
       : super(key: key);
-      
+  @override
+  Widget build(BuildContext context) {
+    //组合上下两部分
+    return Column(
+        children: <Widget>[buildTopRow(context), buildBottomRow(context)]);
+  }
+
+  //创建上半部分
+  Widget buildTopRow(BuildContext context) {
+    return Row(children: <Widget>[
+      Padding(
+          padding: EdgeInsets.all(10),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(model.appIcon, width: 80, height: 80))),
+      Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              model.appName,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 20, color: Color(0xFF8E8D92)),
+            ),
+            Text(
+              "${model.appDate}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 16, color: Color(0xFF8E8D92)),
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+        child: FlatButton(
+          color: Color(0xFFF1F0F7),
+          highlightColor: Colors.blue[700],
+          colorBrightness: Brightness.dark,
+          child: Text(
+            "OPEN",
+            style: TextStyle(
+                color: Color(0xFF007AFE), fontWeight: FontWeight.bold),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          onPressed: onPressed,
+        ),
+      )
+    ]);
+  }
+
+  //创建下半部分
+  Widget buildBottomRow(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(model.appDescription),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text("${model.appVersion} • ${model.appSize} MB"))
+            ]));
+  }
 }
 
 class UpdatedItemModel {
